6893a0a99aaccb01c7eb2bfc5ab2335a83e6e40f
MarkLux <marlx6590@163.com>
2016-08-22 21:25:08 +0800

    简化了文章显示列表

--- application/views/blog_view/list_view.php
+++ application/views/blog_view/list_view.php
@@ -5,13 +5,12 @@
   <div class="row">
 <?php foreach ($article_data as $row): ?>
 
-  <div class="col-sm-6 col-md-4">
+  <div class="col-md-6">
   <div class="thumbnail">
     <h2><?php echo $row['blog_title']; ?></h2>
     <p>
       分类： <a href="<?php echo $base_url;?>index.php/blog/view/<?php echo $row['blog_category']; ?>"><?php echo $row['blog_category']; ?></a> &nbsp &nbsp 时间： <?php echo $row['blog_create_time']; ?> <br>
     </p>
-    <p><?php echo substr($row['blog_md_code'],0,50);?></p>
     <p>
       <a class="btn btn-primary btn-lg" href="<?php echo $base_url?>index.php/article/index/<?php echo $row['blog_id'];?>" role="button">阅读</a>
     </p>
--- application/views/index_view/latest_blogs.php
+++ application/views/index_view/latest_blogs.php
@@ -1,14 +1,13 @@
   <div class="container">
     <div class="row">
   <?php foreach ($blog_data as $row): ?>
-    <div class="col-sm-6 col-md-4">
+    <div class="col-md-6">
     <div class="thumbnail">
 
       <h2><?php echo $row['blog_title']; ?></h2>
       <p>
         分类： <a href="<?php echo $base_url;?>index.php/blog/view/<?php echo $row['blog_category']; ?>"><?php echo $row['blog_category']; ?></a> &nbsp &nbsp 时间： <?php echo $row['blog_create_time']; ?> <br>
       </p>
-      <p><?php echo substr($row['blog_md_code'],0,50);?></p>
       <p>
         <a class="btn btn-primary btn-lg" href="<?php echo $base_url?>index.php/article/index/<?php echo $row['blog_id'];?>" role="button">阅读</a>
       </p>
@@ -18,5 +17,5 @@
   <?php endforeach; ?>
 </div>
 <a class="btn btn-primary btn-lg" href="<?php echo $base_url;?>index.php/blog/view/all">查看全部</a>
-<hr>
+
 </div>
