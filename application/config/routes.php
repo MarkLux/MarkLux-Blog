88a15e00b64f2dff15d4e869a6f1031bfe650381
MarkLux <marlx6590@163.com>
2016-08-28 09:53:58 +0800

    优化路由

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,4 +51,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['default_controller'] = 'hello';
+$route['default_controller'] = 'index';
